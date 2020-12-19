1ee63c17db1658396f71a0948450a2c4e50c1df6
Faimire <faimireg@hotmail.com>
2020-12-19 19:46:58 +0100

    dynamic image center

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from tkinter import Tk, Label, Button, Canvas, PhotoImage
+from tkinter import Tk, Label, PhotoImage, Canvas
+from tkinter.constants import CENTER
 
 class PeepoCam:
+
     def __init__(self, master):
         self.master = master
         master.title("PeepoCam!")
 
         self.image = PhotoImage(file="peepo\\base_shadow_bg.png")
-        self.label = Label(master, image=self.image)
-        self.label.pack()
+        self.canvas = Canvas(master, width = self.image.width(), height = self.image.height())  
+        self.canvas.pack()
+
+        self.canvas.create_image(self.image.width()/2, self.image.height()/2, anchor=CENTER, image=self.image)
+
 
 root = Tk()
 peepoGui = PeepoCam(root)
